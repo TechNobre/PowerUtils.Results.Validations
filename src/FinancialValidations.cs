5f05c80311026bf9bfec6d4efbec284d7badf5dd
Nelson Nobre <technobre@gmail.com>
2022-09-24 17:55:05 +0100

    feat: Added validation for CVV card

---
+++ src/FinancialValidations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace PowerUtils.Results
+{
+    public static class FinancialValidations
+    {
+        /// <summary>
+        /// Most CVVs are 3 digits, but for example American Express has 4 digits
+        /// </summary>
+        private static readonly Regex _cvvRegex = new(@"^\d{3,4}$", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Returns an <see cref="IError" /> if <paramref name="value"/> is not a CVV card
+        /// </summary>
+        public static IError ShouldBeCVV(
+            this string value,
+            Func<IProperty<string>, IError> onError,
+            [CallerArgumentExpression("value")] string propertyName = null
+        )
+        {
+            if(value is null)
+            {
+                return null;
+            }
+
+            if(!_cvvRegex.IsMatch(value))
+            {
+                return onError(new Property<string>(value, propertyName));
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Returns an <see cref="IError" /> if <paramref name="value"/> is not a CVV card. Error code 'INVALID'
+        /// </summary>
+        public static IError ShouldBeCVV(
+            this string value,
+            [CallerArgumentExpression("value")] string propertyName = null
+        ) => value.ShouldBeCVV(
+            (_) => Error.Validation(
+                propertyName,
+                ErrorCodes.INVALID,
+                $"The '{propertyName}' is an invalid CVV format"
+            ),
+            propertyName
+        );
+
+        /// <summary>
+        /// Validates if <paramref name="validatable.Value"/> is not a CVV card and adds an error code 'INVALID' in error list
+        /// </summary>
+        public static IValidatable<string> ShouldBeCVV(this IValidatable<string> validatable)
+            => validatable.Validator(property => property.Value.ShouldBeCVV(property.Name));
+
+        /// <summary>
+        /// Validates if <paramref name="validatable.Value"/> is not a CVV card and adds an error
+        /// </summary>
+        public static IValidatable<string> ShouldBeCVV(
+            this IValidatable<string> validatable,
+            Func<IProperty<string>, IError> onError
+        ) => validatable.Validator(property => property.Value.ShouldBeCVV(onError));
+    }
+}
---
+++ tests/PowerUtils.Results.Validations.Tests/ShouldRules/Financial/ShouldBeCVV.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace PowerUtils.Results.Validations.Tests.ShouldRules.Financial
+{
+    public class ShouldBeCVV
+    {
+        [Fact]
+        public void NULL_ShouldBeCVV_NoErrors()
+        {
+            // Arrange
+            string cvv = null;
+
+
+            // Act
+            var act = cvv.Validate()
+                .ShouldBeCVV();
+
+
+            // Assert
+            act.Errors.Should().HaveCount(0);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("das")]
+        [InlineData("1sd")]
+        [InlineData("cvv")]
+        [InlineData("1")]
+        [InlineData("12")]
+        [InlineData("00")]
+        [InlineData("300d")]
+        [InlineData("10d")]
+        [InlineData("00000")]
+        [InlineData("23424")]
+        [InlineData("12345")]
+        [InlineData(" 123")]
+        [InlineData("123 ")]
+        [InlineData("121.3")]
+        [InlineData("921.3")]
+        public void Invalid_ShouldBeCVV_OneError(string cvv)
+        {
+            // Arrange && Act
+            var act = cvv.Validate()
+                .ShouldBeCVV();
+
+
+            // Assert
+            act.Errors.Should().HaveCount(1);
+
+            act.Errors.Should().OnlyContain(c =>
+                c.Property == nameof(cvv)
+                &&
+                c.Code == ErrorCodes.INVALID
+                &&
+                c.Description == $"The '{nameof(cvv)}' is an invalid CVV format"
+            );
+        }
+
+        [Theory]
+        [InlineData("000")]
+        [InlineData("0000")]
+        [InlineData("001")]
+        [InlineData("444")]
+        [InlineData("4444")]
+        [InlineData("4445")]
+        [InlineData("9874")]
+        public void Valid_ShouldBeCVV_OneError(string cvv)
+        {
+            // Arrange && Act
+            var act = cvv.Validate()
+                .ShouldBeCVV();
+
+
+            // Assert
+            act.Errors.Should().HaveCount(0);
+        }
+
+        [Fact]
+        public void ForbiddenError_ShouldBeCVV_ErrorCode()
+        {
+            // Arrange
+            var countryCode = "cvv";
+
+            var expectedProperty = "fakeProp cvv";
+            var expectedCode = "fakeCode cvv";
+            var expectedDescription = $"Fake description cvv > '{expectedProperty}'";
+
+
+            // Act
+            var act = countryCode.Validate()
+                .ShouldBeCVV(
+                    property => Error.Forbidden(
+                        expectedProperty,
+                        expectedCode,
+                        expectedDescription
+                    )
+                );
+
+
+            // Assert
+            act.Errors.Should().HaveCount(1);
+            act.Errors.First().Should().BeOfType<ForbiddenError>();
+
+            act.Errors.Should().OnlyContain(c =>
+                c.Property == expectedProperty
+                &&
+                c.Code == expectedCode
+                &&
+                c.Description == expectedDescription
+            );
+        }
+    }
+}
