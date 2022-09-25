6a3a88dea3b813e09613d47321000125dc0e48a2
Nelson Nobre <technobre@gmail.com>
2022-09-25 13:04:40 +0100

    feat: Added new method `ShouldBeValidCardExpiryDate()`

--- src/FinancialValidations.cs
+++ src/FinancialValidations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
@@ -10,6 +11,7 @@ namespace PowerUtils.Results
         private const int CARD_NUMBER_MIN_LENGTH = 13;
         private const int CARD_NUMBER_MAX_LENGTH = 19;
 
+        private const string DEFAULT_CARD_EXPIRY_DATE_FORMAT = "yy/MM";
 
 
         /// <summary>
@@ -154,5 +156,86 @@ namespace PowerUtils.Results
             this IValidatable<string> validatable,
             Func<IProperty<string>, IError> onError
         ) => validatable.Validator(property => property.Value.ShouldBeCardNumber(onError));
+
+
+
+        /// <summary>
+        /// Returns an <see cref="IError" /> if <paramref name="value"/> is not a valid card expiry date
+        /// </summary>
+        public static IError ShouldBeValidCardExpiryDate(
+            this string value,
+            Func<IProperty<string>, IError> onErrorExpired,
+            Func<IProperty<string>, IError> onErrorInvalid,
+            string format = DEFAULT_CARD_EXPIRY_DATE_FORMAT,
+            [CallerArgumentExpression("value")] string propertyName = null
+        )
+        {
+            if(value is null)
+            {
+                return null;
+            }
+
+            if(DateTime.TryParseExact(
+                value,
+                format,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out var expiryDate
+            ))
+            {
+                var currentMonth = DateTime.UtcNow;
+                currentMonth = new DateTime(currentMonth.Year, currentMonth.Month, 1);
+
+                if(expiryDate < currentMonth)
+                {
+                    return onErrorExpired(new Property<string>(value, propertyName));
+                }
+
+                return null;
+            }
+
+
+            return onErrorInvalid(new Property<string>(value, propertyName));
+        }
+
+        /// <summary>
+        /// Returns an <see cref="IError" /> if <paramref name="value"/> is not a valid card expiry date. Error code 'MIN:CURRENT_MONTH'
+        /// </summary>
+        public static IError ShouldBeValidCardExpiryDate(
+            this string value,
+            string format = DEFAULT_CARD_EXPIRY_DATE_FORMAT,
+            [CallerArgumentExpression("value")] string propertyName = null
+        ) => value.ShouldBeValidCardExpiryDate(
+            (_) => Error.Validation(
+                propertyName,
+                "MIN:CURRENT_MONTH",
+                $"The '{propertyName}' is an expiry date"
+            ),
+            (_) => Error.Validation(
+                propertyName,
+                ErrorCodes.INVALID,
+                $"The '{propertyName}' is an invalid expiry date format"
+            ),
+            format,
+            propertyName
+        );
+
+        /// <summary>
+        /// Validates if <paramref name="validatable.Value"/> is not a CVV card and adds an error code 'MIN:CURRENT_MONTH' in error list
+        /// </summary>
+        public static IValidatable<string> ShouldBeValidCardExpiryDate(
+            this IValidatable<string> validatable,
+            string format = DEFAULT_CARD_EXPIRY_DATE_FORMAT
+        ) => validatable.Validator(property => property.Value.ShouldBeValidCardExpiryDate(format, property.Name));
+
+        /// <summary>
+        /// Validates if <paramref name="validatable.Value"/> is not a valid card expiry date and adds an error
+        /// </summary>
+        public static IValidatable<string> ShouldBeValidCardExpiryDate(
+            this IValidatable<string> validatable,
+            Func<IProperty<string>, IError> onErrorExpired,
+            Func<IProperty<string>, IError> onErrorInvalid,
+            string format = DEFAULT_CARD_EXPIRY_DATE_FORMAT
+        ) => validatable.Validator(property => property.Value.ShouldBeValidCardExpiryDate(onErrorExpired, onErrorInvalid, format));
     }
 }
